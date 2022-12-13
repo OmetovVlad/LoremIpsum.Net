39669453fa685061b2bfcdf593652e70833a0954
Vladislav Ometov <ometov96@mail.ru>
2022-12-13 09:45:14 +0700

    Готов 2й блок, элементы формы, мобильное меню

--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -14,3 +14,97 @@ export function isWebp() {
 		document.documentElement.classList.add(className);
 	});
 }
+
+export function mobileMenu(){
+	const burgerButton = document.querySelector('.header__burger');
+	const mobileMenu = document.querySelector('.header__menu');
+
+	if (burgerButton) {
+		burgerButton.addEventListener("click", activateMobileMenu);;
+	}
+
+	function activateMobileMenu () {
+		if (this.classList.contains('header__burger--open')){
+			this.classList.remove('header__burger--open');
+			mobileMenu.classList.remove('header__menu--open');
+		} else {
+			this.classList.add('header__burger--open');
+			mobileMenu.classList.add('header__menu--open');
+		}
+	}
+}
+
+export function range(){
+	const ranges = document.querySelectorAll('input[type="range"]');
+
+	if (ranges) {
+		ranges.forEach(function (item) {
+			item.addEventListener("input", rangeInput);
+			return true;
+		});
+	}
+
+	function rangeInput () {
+		let rangeValue = this.value;
+		let rangeInput = this.parentNode;
+
+		// Перемещаем стилизованный ползунок
+		rangeInput.querySelector('.input_range__dot').style.left = rangeValue + "%";
+
+		// Выводим значение
+		let rangeValueHTML = rangeInput.parentNode.querySelector('.input_range__value');
+		rangeValueHTML.textContent = rangeValue + " %";
+
+	}
+}
+
+export function selects(){
+	//Открываем и закрываем селект
+	const selects = document.querySelectorAll('.select__head');
+
+	if (selects) {
+		selects.forEach(function (item) {
+			item.addEventListener("click", selectClick);
+			return true;
+		});
+	}
+
+	function selectClick() {
+		let selected = document.querySelectorAll('.select--open');
+
+		if (this.parentNode.classList.contains('select--open')) {
+			this.parentNode.classList.remove('select--open');
+		} else {
+			if (selected.length > 0) {
+				document.querySelector('.select--open').classList.remove('select--open');
+			}
+			this.parentNode.classList.add('select--open');
+		}
+	}
+
+	//Меняем заголовок и закрываем
+	const selectItems = document.querySelectorAll('.select__item-value');
+
+	if (selectItems) {
+		selectItems.forEach(function (item) {
+			item.addEventListener("click", selectItemClick);
+			return true;
+		});
+	}
+
+	function selectItemClick() {
+		document.querySelector('.select--open').classList.remove('select--open');
+		this.closest('.select').querySelector('.select__title').textContent = this.textContent;
+	}
+
+	//Закрываем по клику вне
+	document.addEventListener( 'click', (e) => {
+		if(document.getElementsByClassName('select--open').length > 0) {
+			const withinBoundaries = e.composedPath().includes( document.querySelector('.select--open') );
+			if(!withinBoundaries){
+				document.querySelector('.select--open').classList.remove('select--open');
+			}
+		}
+	})
+
+}
